22cbc2140debb520d51859c196f512170e60bc27
denis.yudin <rng48322@gmail.com>
2022-08-30 02:22:49 +0300

    First commit

---
+++ src/main/java/Ylab/tasks/task1/Main.java
@@ -0,0 +1,9 @@
+package main.java.Ylab.tasks.task1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ src/main/java/Ylab/tasks/task2/Main.java
@@ -0,0 +1,4 @@
+package main.java.Ylab.tasks.task2;
+
+public class Main {
+}
